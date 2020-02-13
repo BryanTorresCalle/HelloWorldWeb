33320b759f92e274141b3d1c8f0a5d473e4de7e4
BryanTorresCalle <bryk347@gmail.com>
2020-02-13 07:42:47 -0500

    "clase 13/2"

--- frontend/crud/js/estudiantes.js
+++ frontend/crud/js/estudiantes.js
@@ -1,5 +1,9 @@
 
-let estudiantes = [];
+let students = [
+    { id:"1001", name:"Bryan", lastName:"Torres", age:"18", mail:"bryk@gmail.com", career:"001"  }
+];
+
+let tempStudent1 = null;
 
 function getValues() {
     let id = document.getElementById("id").value;    
@@ -16,9 +20,71 @@ function getValues() {
 }
 
 function createStudent() {
-
-    let student = getValues();
-    estudiantes.push(estudiantes);
-    console.log(estudiantes);
     
+    let student = getValues();
+    let existStudent = students.find(s => student.id === s.id)
+    if(existStudent){
+         console.log("El estudiante ya existe");
+         return;
 }
+
+    
+    students.push(student);
+    studentsInList();
+}
+
+function studentsInList() {
+    let list = document.getElementById("listStudents");
+    let data = "";
+    for (let i = 0; i < students.length; i++) {
+        let myStudent = students[i];
+        data += "<tr>"
+        data += `<td>${myStudent.id}</td>`
+        data += `<td>${myStudent.name} ${myStudent.lastName}</td>`
+        data += `<td>${myStudent.mail}</td>`
+        data += `<td>${myStudent.career}</td>`
+        data += `<td><button type="button" onclick="loadInfo(${i})" class="btn btn-primary btn-sm">Editar</button></td>`
+        data += '<td><button type="button" onclick="delStudent('+i+')" class="btn btn-primary btn-sm">Eliminar</button></td>'
+        data += "</tr>"
+    }
+    list.innerHTML = data;
+}
+
+function loadInfo(index){
+    let student = students[index];
+    tempStudent1 = index;
+    document.getElementById("id").value = student.id;
+    document.getElementById("name").value = student.name;
+    document.getElementById("lastName").value = student.lastName;
+    document.getElementById("age").value = student.age;
+    document.getElementById("mail").value = student.mail;
+    document.getElementById("careers").value = student.career;
+
+    document.getElementById("btnCreateStudent").style.display = "none";
+    document.getElementById("btnUpStudent").style.display = "inline";
+}
+
+function delStudent(index) {
+    students.splice(index,1);
+    studentsInList();
+}
+
+function upStudent() {
+    let updatedStudent = getValues();
+    students.splice(tempStudent1,1,updatedStudent);
+    studentsInList();
+    clearInfo();
+    document.getElementById("btnCreateStudent").style.display = "inline";
+    document.getElementById("btnUpStudent").style.display = "none";
+}
+
+function clearInfo() {
+   document.getElementById("id").value  = "";  
+    document.getElementById("name").value  = "";
+    document.getElementById("lastName").value  = "";
+    document.getElementById("age").value  = "";
+    document.getElementById("mail").value  = "";
+    document.getElementById("careers").value  = "";
+    
+   
+}  
