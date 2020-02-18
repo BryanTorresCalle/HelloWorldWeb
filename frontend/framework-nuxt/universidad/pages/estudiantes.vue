43b4019e36a96cc2acbc1baac34e16bf443268b2
BryanTorresCalle <bryk347@gmail.com>
2020-02-18 07:36:15 -0500

    "bustra vue"

---
+++ frontend/framework-nuxt/universidad/pages/estudiantes.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Gestión de estudiantes</h1>
+
+        
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            mensaje:"Hola estudiantes"
+        }
+    }
+}
+</script>
